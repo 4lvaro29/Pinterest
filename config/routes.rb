c6d78f90ed339efb3418109fcc668978081c53a5
Alvaro Urbina <4lvaro.up@gmail.com>
2017-12-26 14:25:17 -0300

    New Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :pins
   get 'pages/index'
 
   get 'pages/about'
 
   get 'pages/contact'
+
+  root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
